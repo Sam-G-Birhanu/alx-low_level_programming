4de86948fc6f46f8c6f52f01547810b4ab18f5f1
cyberSam <samrawitguangulb@gmail.com>
2023-07-18 13:06:30 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
  * print_sign - returns 1 and shows + if the input is positive,
  * returns 0 and shows 0 if 0
  * returns -1 and shows - if the input is negative
- * @c: the character in AsCII mode
+ * @n: the number to be checked
  *
  *  Return: 1 for positive, 0 for zero, -1 for negative
  */
@@ -16,12 +16,12 @@ int print_sign(int n)
 	}
 	else if (n == 0)
         {
-                _putchar('0');
-                return (0);
+		_putchar('0');
+		return (0);
         }
 	else
         {
-                _putchar('+');
+		_putchar('+');
 		return (1);
         }
 	_putchar('\n');
