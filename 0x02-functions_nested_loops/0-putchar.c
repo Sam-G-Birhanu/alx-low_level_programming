029180cf2bbd620548122c259b1aae1766a75728
cyberSam <samrawitguangulb@gmail.com>
2023-07-18 09:38:18 -0700

    putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * main function prints the characters in the function name _putchar **/
+ * main function prints the characters in the function name _putchar 
+ */
 int main(void)
 {
 	_putchar('_');
