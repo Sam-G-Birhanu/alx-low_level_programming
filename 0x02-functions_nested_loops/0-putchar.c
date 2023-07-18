477712003e55f3473d263dfced81c38a04fa3334
cyberSam <samrawitguangulb@gmail.com>
2023-07-18 09:54:17 -0700

    putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * main function prints the characters in the function name _putchar 
+ * main - entry point
+ * Description: prints _putchar to stdout
+ * Return: always 0
  */
 int main(void)
 {
