dc9971779f7dffaabac5be81943bb316d5168e58
cyberSam <samrawitguangulb@gmail.com>
2023-07-18 11:43:37 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * 2-print_alphabet_x10.c - prints the alphabet 10 times
+ *
+ * Return: Always 0.
+ */
+void 2-print_alphabet_x10.c(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
