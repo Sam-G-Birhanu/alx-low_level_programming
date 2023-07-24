bfee77bb489804da6de57665dcdcbfa5da42b1ff
cyberSam <samrawitguangulb@gmail.com>
2023-07-24 12:19:00 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,13 @@
 
 /**
  * _puts - prints a string
+ * @str: is a string pointer
  * Return: void
  */
 
 void _puts(char *str)
 {
-	while(*str != '\0')
+	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
