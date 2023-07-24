cc7805206b3dfeb7fac956d562def729e756aa65
cyberSam <samrawitguangulb@gmail.com>
2023-07-24 07:14:53 -0700

    _strlen(char *s)

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strlen - returns the length of the string
+ * @s: is a variable that represents a char
+ * return: int
+ */
+
+int _strlen(char *s)
+{
+	int leng = 0;
+
+	while (*s != '\0')
+	{
+		leng++;
+		s++;
+	}
+	return (leng);
+}
