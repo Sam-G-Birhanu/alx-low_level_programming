d497b2eed59d2db0f8ce424619a5a2e21b093f76
Sam-G-Birhanu <samrawitguangulb@gmail.com>
2023-08-14 10:11:33 -0700

    1-pi.h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,12 @@
+#ifndef 2-main.c
+#define 2-main.c 
 #include <stdio.h>
 #include <stdlib.h>
 void filename (void)
 {
-	char *filename = __FILE__ ;
+	char filename[] = __FILE__ ;
 
 	printf("%s \n", filename);
 	return;
 }
+#endif
