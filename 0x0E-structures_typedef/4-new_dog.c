28a2aaa362e90f8901ce1f5c3bceb18f2ce98930
Sam-G-Birhanu <samrawitguangulb@gmail.com>
2023-08-15 15:09:07 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,28 +8,30 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ * Return: returns type struct dog
  **/
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog = malloc(sizeof(dog_t));
+
 	if (newDog == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	newDog->name = strdup(name);
 	if (newDog->name == NULL)
 	{
 		free(newDog);
-		return NULL;
+		return (NULL);
 	}
 	newDog->owner = strdup(owner);
 	if (newDog->owner == NULL)
 	{
 		free(newDog->name);
 		free(newDog);
-		return NULL;
+		return (NULL);
 	}
 	newDog->age = age;
-	return newDog;
+	return (newDog);
 }
