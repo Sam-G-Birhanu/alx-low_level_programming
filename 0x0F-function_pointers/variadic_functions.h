cda30e15f86cd2e8fd5976ec03128da452e37328
Sam-G-Birhanu <samrawitguangulb@gmail.com>
2023-08-17 11:05:25 -0700

    variadic_functions.h

--- 0x0F-function_pointers/variadic_functions.h
+++ 0x0F-function_pointers/variadic_functions.h
@@ -1,5 +1,10 @@
-#ifndef FUN_PTR
-#define FUN_PTR
+#ifndef _FUN_PTR
+#define _FUN_PTR_
+
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
 #endif
