511f1e24b8edb28d0f43972346b4caccc1024944
Sam-G-Birhanu <samrawitguangulb@gmail.com>
2023-08-17 11:33:23 -0700

    1-array_iterator.c

--- 0x10-variadic_functions/1-array_iterator.c
+++ 0x10-variadic_functions/1-array_iterator.c
@@ -1,10 +1,18 @@
-#ifndef _FUN_PTR
-#define _FUN_PTR
+#include "function_pointers.h"
 
-#include <stdlib.h>
-
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-
-#endif
+/**
+ * array_iterator - executes a function given as a
+ * parameter on each element of an array.
+ * @array: input integer array.
+ * @size: size of the array.
+ * @action: pointer to the function.
+ *
+ * Return: no return.
+ **/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+	
+	if (array && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
