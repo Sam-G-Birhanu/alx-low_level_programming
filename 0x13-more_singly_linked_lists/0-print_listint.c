e00250fbc87454a3b6392510e477fcff25ae1dd3
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-08-30 16:56:38 +0530

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,43 +1,22 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the loop in a linked list.
- * @head: head of a list.
- *
- * Return: the address of the node where the loop starts.
- */
-listint_t *find_listint_loop(listint_t *head)
+ * print_listint - a function that prints all elements of a list
+ * @h: a pointer that points to a struct listint_t
+ * Return: size_t, number of nodes posted
+ **/
+size_t print_listint(const listint_t *h)
 {
-	listint_t *p2;
-	listint_t *prev;
+	size_t count = 0;
 
-	p2 = head;
-	prev = head;
-	while (head && p2 && p2->next)
+	while (h != NULL)
 	{
-		head = head->next;
-		p2 = p2->next->next;
-
-		if (head == p2)
-		{
-			head = prev;
-			prev =  p2;
-			while (1)
-			{
-				p2 = prev;
-				while (p2->next != head && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == head)
-					break;
-
-				head = head->next;
-			}
-			return (p2->next);
-		}
+		printf("%d", h->n);
+		count++;
+		h = h->next;
 	}
-
+	return (count);
+}
 	return (NULL);
 }
 
