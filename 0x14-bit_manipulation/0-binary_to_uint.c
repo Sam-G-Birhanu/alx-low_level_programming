d22abc2bc13ec1f960860df087a999f132c1ff91
Sam-G-Birhanu <samrawitguangulb@gmail.com>
2023-09-02 23:13:18 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
- * binary_to_uint - takes a number and turns it into its binary form 
+ * binary_to_uint - takes a number and turns it into its binary form
  * @b: a paremeter that takes a string to be converted
  * Return: the converted number, or 0
  **/
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int i;
-    unsigned int sum = 0;
+	unsigned int i;
+	unsigned int sum = 0;
 
-    for (i = _strlen(b) - 1; i != (unsigned int)-1; i--) 
-    {
-        if (b[i] == '1')
-        {
-            sum += 1 << (_strlen(b) - 1 - i);
-        }
-        else if (b[i] != '0')
-        {
-            return 0;
-        }
-    }
- return sum;
+	for (i = _strlen(b) - 1; i != (unsigned int)-1; i--)
+	{
+		if (b[i] == '1')
+		{
+			sum += 1 << (_strlen(b) - 1 - i);
+		}
+		else if (b[i] != '0')
+		{
+			return (0);
+		}
+	}
+	return (sum);
 }
