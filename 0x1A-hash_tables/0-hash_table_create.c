f18c7703cabe47c5925f9d13739b6a6902c3a5f6
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-02 21:51:45 +0530

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,20 +1,25 @@
 #include hash_tables.h
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t* table = (hash_table_t*) malloc(sizeof(hash_table_t));
+    unsigned long int i;
+    hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
     if (table == NULL)
     {
         return NULL;
     }
+
     table->size = size;
-    table->array = (hash_node_t**) calloc(table->size, sizeof(hash_node_t));
+    table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
     if (table->array == NULL)
     {
+        free(table);
         return NULL;
     }
-    for(int i = 0; i < table->size; i++)
+
+    for (i = 0; i < table->size; i++)
     {
         table->array[i] = NULL;
     }
+
     return table;
 }
