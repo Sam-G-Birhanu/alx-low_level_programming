83821d31149e37372306e644d760dadd9bbae420
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-02 21:41:30 +0530

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
     {
         return NULL;
     }
-    for(int i = 0; table->size; i++)
+    for(int i = 0; i < table->size; i++)
     {
         table->array[i] = NULL;
     }
