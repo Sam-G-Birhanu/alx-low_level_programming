68482bf296b7927ef89b01103dce7e22b4e3c87b
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2024-01-02 09:52:11 -0800

    created file

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,13 @@
 #include "hash_tables.h"
-/**key_index - a function that returns index @key: input key @size: size of the table Return: index
+/**
+ * key_index - a function that returns index
+ * @key: input key
+ * @size: size of the table
+ * Return: index
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-  hash = hash_djb2(key);
-return hash % size;
+	hash = hash_djb2(key);
+	return (hash % size);
 }
