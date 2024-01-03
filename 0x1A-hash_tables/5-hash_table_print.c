f51d360e9c3a976f90c368c47272e0e457a3d9b6
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-03 14:40:35 +0530

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -5,24 +5,29 @@ void hash_table_print(const hash_table_t *ht)
     if (ht != NULL)
     {
         long unsigned int i;
-        for (i = 0;i < ht->size;i++)
+        printf("{");
+
+        for (i = 0; i < ht->size; i++)
         {
-            hash_node_t* current = ht->array[i];
-            printf("{'%s': '%s'",current->key, current->value);
-    
-            while(current->next != NULL)
+            hash_node_t *current = ht->array[i];
+
+            while (current != NULL)
             {
-                printf(", ");
+                if (current->key != NULL && current->value != NULL)
+                {
+                    printf("'%s': '%s'", current->key, current->value);
+
+                    if (current->next != NULL)
+                    {
+                        printf(", ");
+                    }
+                }
+
                 current = current->next;
-                printf("'%s': '%s'",current->key, current->value);
             }
         }
-        if (i < ht->size){
-            printf(",");
+
+        printf("}\n");
     }
-        if (i == ht->size){
-            printf("}");
-    }
-        
 }
-}
+
