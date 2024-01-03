287b8f8ea1c1ae3788447d824ec254b71c73a878
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-03 10:53:42 +0530

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,7 +1,6 @@
 #include "hash_tables.h"
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    {
     if(key != NULL)
     {
        unsigned long int index = key_index(key, ht->size);
@@ -24,12 +23,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
        {
            hash_node_t* current = ht->array[index];
            while (current != NULL){
-               if(strcmp(current->key, key))
+               if(strcmp(current->key, key) == 0)
                {
-                   
+                  free(current->value);
+                  current->value = strdup(value);
+                  return 1;
                }
+               current = current->next;
            }
-           
+        hash_node_t* new_node = (hash_node_t*) malloc(sizeof(hash_node_t));
+        if(new_node == NULL)
+            return 0;
+        new_node->key = strdup(key);
+        new_node->value = strdup(value);
+        new_node->next = ht->array[index];
+        ht->array[index] = new_node;
+        return 1;
        }
        }
     else
