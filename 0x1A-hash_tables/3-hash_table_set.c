c1c261eb7889c53da23c0be0eb874e67fcb24b07
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-03 09:55:51 +0530

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,1 +1,26 @@
-#include hash_tables.h 
+#include hash_tables.h
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    if(key != NULL)
+    {
+       unsigned long int index = key_index(key, ht->size);
+       if (index > ht->size)
+       {
+           return 0;
+       }
+       if (ht->array[index] == NULL)
+       {
+           array[index] = value;
+           return 1;
+       }
+       else
+       {
+           *array[index]->next = value;
+           return 1;
+       }
+       }
+    else
+       {
+           return 0;
+       }
+}
