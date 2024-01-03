9e2fa121bdb52bbca5535b5b1e43fbecff170407
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-03 15:06:17 +0530

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,4 +1,28 @@
 #include "hash_tables.h"
 
 void hash_table_delete(hash_table_t *ht){
+  free_item(new_node);
+  free_table(ht);
 } 
+void free_item(Ht_item* item)
+{
+    free(item->key);
+    free(item->value);
+  free(item->next);
+    free(item);
+}
+
+void free_table(HashTable* table)
+{
+
+    for (int i = 0; i < table->size; i++)
+    {
+        Ht_item* item = table->array[i];
+
+        if (item != NULL)
+            free_item(array);
+    }
+
+    free(table->array);
+    free(table);
+}
