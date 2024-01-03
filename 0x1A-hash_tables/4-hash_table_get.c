fa1da319c939ebc80da7289cbd46c37c5c01d916
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-03 11:40:18 +0530

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,1 +1,19 @@
 #include hash_tables.h
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+  if (ht != NULL && key != NULL)
+{
+  unsigned long int index = key_index((const unsigned char*)key, ht->size);
+  hash_node_t* current = ht->array[index];
+  if (current == NULL || current->value == NULL)
+  return NULL;
+  else
+  return (current->value)
+  
+}
+else
+{
+    return NULL;
+}
+}
