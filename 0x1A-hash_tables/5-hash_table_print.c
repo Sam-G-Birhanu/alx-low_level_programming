d45ca8781f16a0432b7794def9761dac72dc80d4
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-01-03 12:09:33 +0530

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,1 +1,19 @@
-#include hash_tables.h
+#include "hash_tables.h"
+
+void hash_table_print(const hash_table_t *ht)
+{
+    if (ht != NULL)
+    {
+        int i = 0;
+        while(i < ht->size)
+        {
+            hash_node_t* current = ht->array[i];
+            print("%s: %s",current->key, current->value);
+            while(current->next != NULL)
+            {
+                current = current->next;
+                print(", %s: %s",current->key, current->value);
+            }
+        }
+    }
+}
