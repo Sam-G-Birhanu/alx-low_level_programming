fef751b7ead67bb2ae92082a40abb3a952d107e0
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-03-13 16:05:01 +0530

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,1 +1,30 @@
 #include "search_algos.h"
+
+int binary_search(int *array, size_t size, int value)
+{
+    size_t left = 0;
+    size_t right = size - 1;
+    
+    if (array == NULL)
+        return -1;
+    while (left <= right) {
+    for (int i = left; i <= right; i++)
+       {
+           if(array[i] == right)
+                printf("%d", array[i]);
+            else
+                printf("%d, ", array[i]); 
+       }
+       printf("\n");
+        size_t middle = left + (right - left) / 2;
+
+        if (array[middle] == value)
+            return middle;
+        else if (array[middle] < value)
+            left = middle + 1;
+        else
+            right = middle - 1;
+    }
+
+    return -1;
+}
