84531ebd449d2774f07b9ade24543193ef6fbbfa
Sam-G-Birhanu <21btrcs228@jainuniversity.ac.in>
2024-03-13 03:45:37 -0700

    fixed betty style

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,30 +1,37 @@
 #include "search_algos.h"
+/**
+ * binary_search - performs binary search
+ * @array: pointer to the first element of the array
+ * @size: size of the array
+ * @value: value to be searched
+ * Return: returns the index of value
+ */
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    
-    if (array == NULL)
-        return -1;
-    while (left <= right) {
-    for (int i = left; i <= right; i++)
-       {
-           if(array[i] == right)
-                printf("%d", array[i]);
-            else
-                printf("%d, ", array[i]); 
-       }
-       printf("\n");
-        size_t middle = left + (right - left) / 2;
+	size_t left = 0;
+	size_t right = size - 1;
 
-        if (array[middle] == value)
-            return middle;
-        else if (array[middle] < value)
-            left = middle + 1;
-        else
-            right = middle - 1;
-    }
+	if (array == NULL)
+		return (-1);
+	while (left <= right)
+	{
+		for (int i = left; i <= right; i++)
+		{
+			if (array[i] == right)
+				printf("%d", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+		printf("\n");
+		size_t middle = left + (right - left) / 2;
 
-    return -1;
+		if (array[middle] == value)
+			return (middle);
+		else if (array[middle] < value)
+			left = middle + 1;
+		else
+			right = middle - 1;
+	}
+	return (-1);
 }
