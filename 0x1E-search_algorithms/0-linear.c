402fb6390ab745dff28c323319b01c47c818522b
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-03-14 16:24:06 +0530

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,9 +17,9 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, *array);
 		if (*array == value)
 		{
+			printf("Value checked array[%ld] = [%d]\n", i, *array);
 			return ((int)i);
 		}
 			printf("Value checked array[%ld] = [%d]\n", i, *array);
